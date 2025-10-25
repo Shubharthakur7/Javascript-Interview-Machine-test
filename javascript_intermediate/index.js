0b008f8b34890fe081eae05aa7ddbd117f6c4932
Shubham Singh <shubhamrsingh8@gmail.com>
2025-10-25 06:47:05 +0530

    added advance js

--- javascript_intermediate/index.js
+++ javascript_intermediate/index.js
@@ -1,1 +1,61 @@
+// 🟡 Intermediate (Array & Object Manipulation – 11 to 25)
+
+let results = result => console.log(result());
+
 // 1-Flatten a nested array (e.g. [1, [2, [3, 4]]] → [1,2,3,4]).
+let arr= [1, [2, [3, 4]]];
+const flattenArr = (arr) => {
+    let results = [];
+    for(let i=0; i<arr.length; i++) {
+        if(Array.isArray(arr[i])) {
+            // results.push(...flattenArr(arr[i]));
+            results = results.concat(flattenArr(arr[i]))
+        } else {
+            results.push(arr[i])
+        }
+    }
+    return results
+}
+console.log(flattenArr(arr));
+
+// 2-Find the intersection of two arrays.
+let arr1 = [3,4,5,9,10,11,12];
+let arr2 = [4,5,6,7];
+const intersection = (arr1, arr2) => {
+    let n1 = arr1.length;
+    let n2 = arr2.length;
+    let n, arr, resArr;
+    let results=[];
+    if(n1 > n2) {
+        n= n2;
+        arr = arr2;
+        resArr = arr1;
+    } else {
+        n = n1;
+        arr = arr1;
+        resArr= arr2
+    }
+
+    for(let i=0; i<arr.length; i++) {
+        let r = arr[i];
+        if(resArr.includes(r)) {
+            results.push(r);
+        }
+    }
+    return results;
+}
+console.log(intersection(arr1, arr2));
+
+// 3- Sort an array of objects by a specific property.
+arr = [{id: 5, name:"shubham"}, {id: 2, name:"rahul"}, {id: 3, name:"akash"}, {id: 4, name:"lokesh"}]
+
+const sortArr = (arr, el) => {
+    let result = arr.sort((a,b) => {
+        if(typeof a[el] === "string") {
+            return a[el].localeCompare(b[el])
+        }
+        return a[el] - b[el]});
+    return result;
+}
+
+console.log(sortArr(arr, "name"));
